0eb6ad9e81ad5621a9ef84ca759e784d47c9f174
Belfodil Abdessamad <47505556+BelfoSamad@users.noreply.github.com>
2024-11-23 10:09:08 +0100

    feat: add auth logic implementation

--- extension/src/offscreen/script.js
+++ extension/src/offscreen/script.js
@@ -6,3 +6,46 @@ import {getFunctions, httpsCallable} from 'firebase/functions';
 const auth = getAuth(app);
 const db = getFirestore(app);
 const functions = getFunctions(app);
+
+// listen to auth state
+auth.onAuthStateChanged((user) => {
+    chrome.runtime.sendMessage({target: "background", action: "userIn", isLoggedIn: user != null});
+});
+
+// Listen for messages from sidepanel & background service worker
+chrome.runtime.onMessage.addListener(handleChromeMessages);
+function handleChromeMessages(message, _sender, sendResponse) {
+    if (message.target == "offscreen") switch (message.action) {
+        //--------------------------- Authentication
+        case "register":
+            (async () => {
+                try {
+                    let userCreds = await createUserWithEmailAndPassword(auth, message.email, message.password);
+                    await setDoc(doc(db, "users", userCreds.user.uid), defaults);
+                    sendResponse({done: true});
+                } catch (e) {
+                    sendResponse({done: false, error: e.message});
+                }
+            })();
+            return true;
+        case "login":
+            (async () => {
+                try {
+                    let userCreds = await signInWithEmailAndPassword(auth, message.email, message.password);
+                    getDoc(doc(db, "users", userCreds.user.uid)).then(async querySnapshot => {
+                        if (querySnapshot.exists()) chrome.storage.local.set({articleIds: JSON.stringify(querySnapshot.data()['articleIds'])});
+                        sendResponse({done: true});
+                    });
+                } catch (e) {
+                    sendResponse({done: false, error: e.message});
+                }
+            })();
+            return true;
+        case "isLoggedIn":
+            sendResponse({isLoggedIn: auth.currentUser != null});
+            break;
+        case "logout":
+            auth.signOut();
+            break;
+    }
+}
--- extension/src/sidepanel/angular/src/app/services/auth.service.ts
+++ extension/src/sidepanel/angular/src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+///<reference types="chrome"/>
 import {Injectable} from '@angular/core';
 
 @Injectable({
@@ -7,19 +8,30 @@ export class AuthService {
 
   async register(email: string, password: string) {
     return new Promise((resolve) => {
+      chrome.runtime.sendMessage({target: "offscreen", action: "register", email: email, password: password}, (response) => {
+        resolve(response);
+      });
     });
   }
 
   async login(email: string, password: string) {
     return new Promise((resolve) => {
+      chrome.runtime.sendMessage({target: "offscreen", action: "login", email: email, password: password}, (response) => {
+        resolve(response);
+      });
     });
   }
 
   async isLoggedIn() {
     return new Promise((resolve) => {
+      chrome.runtime.sendMessage({target: "offscreen", action: "isLoggedIn"}, (response) => {
+        resolve(response.isLoggedIn);
+      });
     });
   }
 
-  async logout() { }
+  async logout() {
+    return await chrome.runtime.sendMessage({target: "offscreen", action: "logout"});
+  }
 
 }
