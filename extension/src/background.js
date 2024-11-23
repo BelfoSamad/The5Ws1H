2250f340ef06c4f71b8c3543ce3be55d132b3ec6
Belfodil Abdessamad <47505556+BelfoSamad@users.noreply.github.com>
2024-11-23 10:53:38 +0100

    feat: create offscreen script from background

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,5 +1,33 @@
 //------------------------------- Declarations
+let creating; // A global promise to avoid concurrency issues
 
 //------------------------------- Starting
 setupOffscreenDocument("./offscreen/offscreen.html");
 chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true}).catch((error) => console.error(error));
+
+//------------------------------- Handle Offscreen Documents
+async function setupOffscreenDocument(path) {
+    // Check if there is offscreen document with the given path
+    const offscreenUrl = chrome.runtime.getURL(path);
+    const existingContexts = await chrome.runtime.getContexts({
+        contextTypes: ['OFFSCREEN_DOCUMENT'],
+        documentUrls: [offscreenUrl]
+    });
+
+    if (existingContexts.length > 0) {
+        return;
+    }
+
+    // create offscreen document
+    if (creating) {
+        await creating;
+    } else {
+        creating = chrome.offscreen.createDocument({
+            url: path,
+            reasons: ['DOM_SCRAPING'],
+            justification: 'this document is used to communicate with firebase (Auth, Firestore, Functions)',
+        });
+        await creating;
+        creating = null;
+    }
+}
