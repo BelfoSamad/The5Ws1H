3d7d0ec790eb95f243eb2acee1f42c16cc50f3ec
Belfodil Abdessamad <47505556+BelfoSamad@users.noreply.github.com>
2024-11-23 12:01:12 +0100

    feat: add overlay scanning animation

---
+++ extension/src/content/script.js
@@ -0,0 +1,19 @@
+// Create Animated Overlay
+function createOverlay() {
+    const overlay = document.createElement("div");
+    overlay.className = "the5ws1h-overlay";
+    document.body.appendChild(overlay);
+}
+
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
+    switch (message.action) {
+        case "start_animation":
+            createOverlay();
+            break;
+        case "stop_animation":
+            const overlaysByClass = document.getElementsByClassName("the5ws1h-overlay");
+            if (overlaysByClass.length > 0) document.body.removeChild(overlaysByClass[0]);
+            break;
+    }
+});
